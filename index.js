532219fde04c3f03bde9c0625ac22359d83ca4f7
gunkaran singh <101516987+Gunkaransingh@users.noreply.github.com>
2022-07-08 18:01:02 +0530

    node

--- index.js
+++ index.js
@@ -2,23 +2,33 @@
 
 // weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result) {
 //     if(err) console.log(err);
-   
+
 //     console.log(JSON.stringify(result, null, 2));
 //   });
 
 const express = require('express')
 const app = express()
-
+app.use(express.urlencoded({ extended: false }))
 app.get('/', function (req, res) {
-  res.send('Hello World')
-  console.log('check');
+  // res.send('Hello World')
+  // console.log('check');
 })
-app.post('/getuser',function(req,res){
-var data={
-    'name':'gunkaran singh',
-    'email':'gunkaran@gmail.com',
-    'contact':9876543210
-}
-res.json({'success':true,'status':200,'data':data})
+app.post('/getuser', function (req, res) {
+  var data = {
+    'name': 'gunkaran singh',
+    'email': 'gunkaran@gmail.com',
+    'contact': 9876543210
+  }
+  
+  res.json({ 'success': true, 'status': 200, 'data': data })
 })
+
+
+app.post('/adduser', function(req,res){
+  // console.log(req.body)
+  var data=req.body
+  res.json({'success':true,'status':200,'data':data})
+})
+var studentroute=require('./routes/studentroutes')
+app.use('/student',studentroute)
 app.listen(3000)
